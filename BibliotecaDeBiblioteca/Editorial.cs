ece3bea6dcc45b957625914802adee2ab1042d9d
Renato Jara <renatojara@live.com>
2019-05-19 23:05:04 -0400

    Cambios en el frmLibros

--- BibliotecaDeBiblioteca/Editorial.cs
+++ BibliotecaDeBiblioteca/Editorial.cs
@@ -45,3 +45,4 @@ namespace BibliotecaDeBiblioteca
             return this.nombre;
         }
     }
+}
--- W1/frmLibro.cs
+++ W1/frmLibro.cs
@@ -18,9 +18,34 @@ namespace W1
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void frmLibro_Load(object sender, EventArgs e)
         {
-            
+            cmbGenero.DataSource = Enum.GetValues(typeof(Genero));
+            cmbIdioma.DataSource = Enum.GetValues(typeof(Idioma));
+            cmbIdioma.SelectedItem = null;
+            cmbGenero.SelectedItem = null;
+            dgvLibro.AutoGenerateColumns = true;
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            Libros book = CargaLibro();
+            Libros.listalibros.Add(book);
+        }
+        private Libros CargaLibro()
+        {
+            Libros libro = new Libros();
+            libro.titulo = txtTitulo.Text;
+            libro.ISBN = txtISBN.Text;
+            libro.año = Convert.ToInt16(txtAñoPublicacion.Text);
+            libro.idioma = (Idioma)cmbIdioma.SelectedItem;
+            libro.genero = (Genero)cmbGenero.SelectedItem;
+            return libro;
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
