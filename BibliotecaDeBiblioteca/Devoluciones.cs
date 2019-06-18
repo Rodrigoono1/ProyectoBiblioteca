13b3f53bab7899c1ed04502174c98e45c1bae291
Diego Benítez <diego_db_88@hotmail.com>
2019-06-17 23:02:31 -0400

    Se agregaron atributos a la clase devolucion y se agregaron campos al formulario devoluciones.

--- BibliotecaDeBiblioteca/Devoluciones.cs
+++ BibliotecaDeBiblioteca/Devoluciones.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace BibliotecaDeBiblioteca
 {
-    class Devoluciones
+    public class Devoluciones
     {
+        public int Id { get; set; }
+        public string Socio { get; set; }
+        public string Libro { get; set; }
+        public string Fecha_Prestamo { get; set; }
+        public string Fecha_Devolucion { get; set; }
+
+        public Devoluciones() { }
+       
+        public List<Devoluciones> listardevoluciones = new List<Devoluciones>();
+
+        public static List<Devoluciones> listaDevolucion = new List<Devoluciones>();
+
+        public static List<Devoluciones> ObtenerDevoluciones()
+        {
+            Devoluciones devolucion;
+            listaDevolucion.Clear();
+
+            using (SqlConnection con = new SqlConnection(SqlServer.CADENA_CONEXION))
+            {
+                con.Open();
+                string textoCmd = "Select * from Devoluciones";
+
+                SqlCommand cmd = new SqlCommand(textoCmd, con);
+
+                SqlDataReader elLectorDeDatos = cmd.ExecuteReader();
+
+                while (elLectorDeDatos.Read())
+                {
+                    devolucion = new Devoluciones();
+                    devolucion.Id = elLectorDeDatos.GetInt32(0);
+                    devolucion.Socio = elLectorDeDatos.GetString(1);
+                    devolucion.Libro = elLectorDeDatos.GetString(2);
+                    devolucion.Fecha_Prestamo = elLectorDeDatos.GetString(3);
+                    devolucion.Fecha_Devolucion = elLectorDeDatos.GetString(4);
+
+
+                    listaDevolucion.Add(devolucion);
+                }
+            }
+            return listaDevolucion;
+        }
     }
 }
+
